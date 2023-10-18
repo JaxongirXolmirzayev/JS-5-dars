b7ce2b17f0bef5cb31325fcf2e43af4185bdb462
AhadullayevAbdulloh <147791264+JaxongirXolmirzayev@users.noreply.github.com>
2023-10-18 18:16:04 +0500

    JS 5-dars
    27-guruh

---
+++ JS 5-dars/JS/main.js
@@ -0,0 +1,52 @@
+/* DOM IN JS */
+
+// let headTag = document.head;
+// let bodyTag = document.body;
+// let havola = document.documentURI;
+// let formalar = document.forms;
+// let havolalar = document.links;
+// console.log(havolalar);
+
+/* Events in JS (JSda Hodisalar)*/
+
+// function meniBirMartabos(){
+//     alert("Siz batton bir marta cbosdingiz")
+// }
+//
+// function meniIkkiMartaBos(){
+//     alert("Siz Battoni ikki marta bosdingiz")
+// }
+//
+// function tanlash(event){
+//     alert( event.target.value);
+//     alert("Siz" + event.target.value + "ni tanladiz");
+// }
+//
+// function inputgaBosish(){
+//     console.log("Inputga bosildi")
+// }
+//
+// function inputdanChiqish(){
+//     console.log("Inputdan chiqildi!")
+// }
+//
+// function yozish(event){
+//     console.log(event.target.value)
+// }
+// document.querySelector(".matnTegi").addEventListener('copy', function (event){
+//     console.log("Diqqat! Diqqat! Sizdan " + event.target.value + " nomli xujjat ko'chirilyabdi!")
+// })
+//
+// function kirish(){
+//     console.log("Hududga Kirildi")
+// }
+// function chiqish(){
+//     console.log("Hududdan chiqildi!")
+// }
+// function harakatlanish(e){
+//     console.log(e.clientX,e.clientY)
+// }
+//
+// document.querySelector(".ochirildi").addEventListener('reading', function (event){
+//     console.log("Diqqat! Diqqat! Sizdan " + event.target.value + " nomli xujjat ochirildi")
+// })
